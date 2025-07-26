7f9238beb88bcb47c791a42833905b10c43406a0
rajamails19 <rajamails19@gmail.com>
2025-07-26 14:42:11 -0400

    Initial sentiment analyzer app

---
+++ app.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+import gradio as gr
+
+# Load the Hugging Face model
+classifier = pipeline("sentiment-analysis")
+
+# Function to use in the web app
+def analyze_sentiment(text):
+    result = classifier(text)[0]
+    label = result['label']
+    score = round(result['score'], 2)
+    return f"{label} (Confidence: {score})"
+
+# Build Gradio Interface
+interface = gr.Interface(
+    fn=analyze_sentiment,
+    inputs="text",
+    outputs="text",
+    title="Sentiment Analyzer 🤖",
+    description="Enter a sentence and see if it's Positive or Negative!"
+)
+
+# Run the web app locally
+interface.launch()
